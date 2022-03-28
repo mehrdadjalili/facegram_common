4a57b01fb04fa3c6b2244f304a87c68ae316f9cb
mehrdad jalili <mehrdadjalili2626@gmail.com>
2022-03-28 23:30:00 +0430

    fixed derrors

--- pkg/derrors/common.go
+++ pkg/derrors/common.go
@@ -1,22 +1,11 @@
 package derrors
 
 import (
-	"github.com/mehrdadjalili/facegram_common/pkg/logger"
 	"github.com/mehrdadjalili/facegram_common/pkg/translator/messages"
 )
 
 func InternalError() error {
 	return New(StatusInternalServerError, messages.InternalServerError)
-}
-
-func InternalErrorWithLogger(function string, err error, logger logger.Logger) error {
-	return NewWithLogger(
-		StatusInternalServerError,
-		messages.InternalServerError,
-		logger,
-		function,
-		err,
-	)
 }
 
 func BadRequest() error {
